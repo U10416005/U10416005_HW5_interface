743e612bfdbdb3fa0e4390bccf07731a2487efaf
U10416005 <r3r346@gmail.com>
2016-03-20 20:31:19 +0800

    Import javafx and write class and method
    Import many javafx packages and write main class and main method

---
+++ BeanGame.java
@@ -0,0 +1,21 @@
+//U10416005ªL«Ø¦t
+
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class BeanGame extends Application{
+	@Override
+	public void start(Stage primaryStage){
+		
+	}
+}
